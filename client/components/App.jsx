0d8ab7e234fcb7a3d71e40307dd58bf8d757b4eb
steve-kim-96 <soungku@hotmail.com>
2021-02-05 11:59:17 +1300

    update current changes on main

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 
-<<<<<<< HEAD
+
 import ChristoSmarty from './ChristoSmarty'
-=======
-import ChristoDummy from './ChristoDummy'
 import StylesSmarty from './StylesSmarty'
 import PotentialMatches from './PotentialMatches'
->>>>>>> main
+
 
 const App = () => {
   return (
     <>
 
-<<<<<<< HEAD
+
       <h1>WE GETTING SPICY BAYBEE</h1>
       {/* <Route path='/test' component={ChristoSmarty}/> */}
-=======
+
       <PotentialMatches />
-      <Route path='/test' component={ChristoDummy}/>
+      <Route path='/test' component={ChristoSmarty}/>
       <Route path='/styles' component={StylesSmarty} />
->>>>>>> main
+
 
     </>
   )
